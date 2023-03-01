5f3e0c3c4f98e8ddfc1c90c4d413b79ee3502348
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-03-01 08:41:36 +0000

    [cms] added strapi support along with an example

---
+++ apps/cms/src/api/timeline/controllers/timeline.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * timeline controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::timeline.timeline');
--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -8,8 +8,6 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
