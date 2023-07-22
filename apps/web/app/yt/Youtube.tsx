0ac0a04987f608e00a63a56124da1e989fc83843
Vipin Chaudhary <VipinChaudhary1809@gmail.com>
2023-07-22 08:51:01 +0000

    feat(yt): :sparkles: add sort and count

--- apps/web/app/yt/Youtube.tsx
+++ apps/web/app/yt/Youtube.tsx
@@ -1,52 +1,64 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import YTvideoPlayer from './YTvideoPlayer';
-import { getPlaylistSnippet } from './FetchPlaylist';
-
-interface IYTvideo {
-  videoId: string;
-  videoPublishedAt: string;
-}
+import getPlaylistSnippet from './FetchPlaylist';
+import sortData from './SortVideos';
 
 interface IYoutubePlaylistProps {
   playlistId: string;
   height: string;
   width: string;
   title: string;
-  sorted: boolean;
   API_Key: string | undefined;
+  count?: number;
+  sort?: boolean;
 }
 
-const YoutubePlaylist: React.FC<IYoutubePlaylistProps> = ({ playlistId, height, width, title, sorted, API_Key }) => {
-  const [videos, setVideos] = useState<IYTvideo[]>([]);
+interface IVideoData {
+  kind: string;
+  etag: string;
+  id: string;
+  statistics: {
+    commentCount: string;
+    favoriteCount: string;
+    likeCount: string;
+    viewCount: string;
+  };
+}
+
+const YoutubePlaylist: React.FC<IYoutubePlaylistProps> = ({
+  playlistId,
+  height,
+  width,
+  title,
+  API_Key,
+  count,
+  sort,
+}) => {
+  const [videos, setVideos] = useState<IVideoData[]>([]);
 
   useEffect(() => {
     const fetchVideos = async () => {
       const fetchedVideos = await getPlaylistSnippet(playlistId, API_Key);
       setVideos(fetchedVideos);
+
+      if (sort) {
+        const sortedVideos = await sortData(fetchedVideos);
+        setVideos(sortedVideos);
+      }
     };
 
     fetchVideos();
-  }, [playlistId, API_Key]);
+  }, [playlistId, API_Key, sort]);
 
-  let sortedVideos: IYTvideo[];
-
-  if (sorted) {
-    sortedVideos = videos.sort((a, b) => {
-      const aDate = new Date(a.videoPublishedAt).getTime();
-      const bDate = new Date(b.videoPublishedAt).getTime();
-      return bDate - aDate;
-    });
-  } else {
-    sortedVideos = videos;
-  }
+  const displayedVideos = count ? videos.slice(0, count) : videos;
 
   return (
     <div className="container mx-auto p-8">
       <h1 className="text-3xl mb-6 text-center text-white">{title}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {sortedVideos.map((video: IYTvideo) => (
-          <YTvideoPlayer key={video.videoId} video={video} width={width} height={height} />
+      <div className="space-y-10">
+        {displayedVideos.map((video: IVideoData) => (
+          <YTvideoPlayer key={video.id} videoId={video.id} width={width} height={height} />
         ))}
       </div>
     </div>
--- apps/web/app/yt/page.tsx
+++ apps/web/app/yt/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 //Local 
-// import YoutubePlaylist from './Youtube';
+import YoutubePlaylist from './Youtube';
 
 // Library 
-import YoutubePlaylist from "youtube/Youtube";
+// import YoutubePlaylist from "youtube/Youtube";
 // import { getPlaylistSnippet } from "youtube/functions";
 
 // import "youtube/styles.css";
@@ -13,14 +13,16 @@ const Home = async () => {
   const playlistId = 'PLee3gqXJQrFXj3VqMruL_uC-7pb21cG3g';
   const API_Key = process.env.GOOGLE_API_KEY;
 
+  
 
   return (
     <YoutubePlaylist
       playlistId={playlistId}
+      count = {4}
+      sort = {true}
       height={"200px"}
       width={"450px"}
-      title={"Title for Videos"}
-      sorted={true}
+      title={"Top GSOC Videos 2022"}
       API_Key={API_Key}
     />
   );
