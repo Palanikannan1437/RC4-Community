b5202224b76c4f03830f7c9b7b44147f4c55d2c1
Vipin Chaudhary <VipinChaudhary1809@gmail.com>
2023-07-22 08:49:16 +0000

    feat(yt): :sparkles: fetch each video info

--- apps/web/app/yt/FetchPlaylist.tsx
+++ apps/web/app/yt/FetchPlaylist.tsx
@@ -3,16 +3,51 @@ interface IYTvideo {
     videoPublishedAt: string;
 }
 
-export const getPlaylistSnippet = async (playlistId: string, API_Key: string | undefined): Promise<IYTvideo[]> => {
+interface IVideoData {
+    kind: string;
+    etag: string;
+    id: string;
+    statistics: {
+        commentCount: string;
+        favoriteCount: string;
+        likeCount: string;
+        viewCount: string;
+    };
+}
+
+const getPlaylistSnippet = async (playlistId: string, API_Key: string | undefined)=> {
+// :Promise<IYTvideo[]>
+
+    let videos: IYTvideo[] = [];
     try {
         const response = await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${playlistId}&maxResults=50&key=${API_Key}`);
         const data = await response.json();
         const { items } = data;
-        const contentDetails: IYTvideo[] = items.map((item: any) => item.contentDetails);
-        return contentDetails;
+        videos = items.map((item: any) => ({
+            videoId: item.contentDetails.videoId,
+            videoPublishedAt: item.contentDetails.videoPublishedAt,
+        }));
     } catch (error) {
         console.log(error);
         return [];
     }
+
+    const videoDataPromises = videos.map(async (video) => {
+        const videoID = video.videoId;
+        try {
+            const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoID}&key=${API_Key}`);
+            const data = await response.json();
+            const { items } = data;
+            const videodata: IVideoData = items[0];
+            return videodata;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    });
+
+    const videoDataResults = await Promise.all(videoDataPromises);
+    return videoDataResults.filter((data) => data !== null) as IVideoData[];
 };
 
+export default getPlaylistSnippet;
