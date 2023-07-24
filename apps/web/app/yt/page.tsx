82072d8f3ef18f614514b2287af4fd5af17ca08d
Vipin Chaudhary <VipinChaudhary1809@gmail.com>
2023-07-24 09:07:36 +0000

    feat: :sparkles: add different function to fetch the videos

--- apps/web/app/yt/page.tsx
+++ apps/web/app/yt/page.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 import YoutubePlaylist from './Youtube';
+import getPlaylistSnippet from './FetchPlaylist';
 
 
 const Home = async () => {
-  
   const playlistId = 'PLee3gqXJQrFXj3VqMruL_uC-7pb21cG3g';
   const API_Key = process.env.GOOGLE_API_KEY;
+  
+  const PlayListData =  await getPlaylistSnippet(playlistId,API_Key)
+
 
   return (
     <YoutubePlaylist
-      playlistId={playlistId}
-      count = {4}
+      PlayListData={PlayListData}
+      count = {3}
       sort = {true}
       height={"300px"}
       width={"550px"}
       title={"Top GSOC 2022 Videos"}
-      API_Key={API_Key}
     />
   );
 };
